0b6347648f7e2ba3d5731869895272365839f455
kavtara <kavtara123@gmail.com>
2019-08-11 22:10:39 +0400

    Scroll
    Scroll Top Button

--- Assets/JS/Jscript.js
+++ Assets/JS/Jscript.js
@@ -16,18 +16,17 @@ $(document).ready(function(){
 
 
 // ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 100                       // Scroll to top of body
-    }, 250);
 
-});
 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
 });
